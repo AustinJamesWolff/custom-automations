b3c62fd9f14696ba93dea94e4e35d65796677e97
Austin Wolff <85139314+AustinJamesWolff@users.noreply.github.com>
2021-06-01 00:18:41 -0700

    Add files via upload

---
+++ customValues_for_HighLevel.py
@@ -0,0 +1,15 @@
+# This script takes a contact's HighLevel CRM data
+# and converts the key-value pairs into a list
+# which can then output the single custom value
+# we need, the contact's Lead Origing List
+
+# Example key-value pairs below from the webhook
+input_data = {'customValue': 'Peyronies Disease,YouTubeAds,2021'}
+
+# Splits the customValue's strings into a list
+chunks = input_data['customValue'].split(',')
+
+print(chunks)  # Tests output for developer of split strings
+print(chunks[1])  # Tests output for developer for our desired list value
+
+output = {'Lead Origin List': chunks[1]}
